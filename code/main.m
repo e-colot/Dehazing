0f1238ef8b840259e8ba4902fe3e3efb733b42f1
E. Colot <emmeran.colot@gmail.com>
2025-06-09 11:03:54 +0200

    compiled report

--- code/main.m
+++ code/main.m
@@ -1,6 +1,6 @@
 clear; close all; clc;
 
-image = im2double(imread('./../srcImages/hazy4.jpg'));
+image = im2double(imread('./../srcImages/hazy1.png'));
 
 Ahat = AirlightDirection(image);
 
@@ -20,13 +20,17 @@ distance = distance - min(distance(:));
 distance = distance / max(distance(:)); % Normalize to [0, 1]
 
 figure;
-subplot(1, 3, 1);
+subplot(2, 2, 1);
 imshow(image);
 title('Original Hazy Image');
-subplot(1, 3, 2);
+subplot(2, 2, 2);
 imshow(output);
 title('Dehazed Image');
-subplot(1, 3, 3);
+subplot(2, 2, 3);
 imshow(transmission);
 colormap("jet");
 title('Estimated Transmission Map');
+subplot(2, 2, 4);
+imshow(distance);
+colormap("jet");
+title('Distance Map');
